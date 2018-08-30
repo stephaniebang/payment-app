b3995f3d640072d844abfd4c34612036c99a858c
stephaniebang <ej.bang27@gmail.com>
2018-08-29 23:10:26 -0300

    Add more dummy data and change font

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,26 @@
 
     data() {
       return{
+        // Dummy data: tables with dishes are 1, 5 and 7
         tables: [
           { n: 1,
             dishes: [
               { name: 'Filé de Frango Acebolado',
-                total: 22.60, left: 22.60, paying: 0, selected: false },
+                total: 22.60, left: 22.60, paying: 0, selected: false, history: [] },
               { name: 'Berinjela Recheada com Feijão Branco',
-                total: 24.00, left: 24.00, paying: 0, selected: false },
+                total: 24.00, left: 24.00, paying: 0, selected: false, history: [] },
               { name: 'Creme de Espinafre',
-                total: 15.50, left: 15.50, paying: 0, selected: false },
+                total: 15.50, left: 15.50, paying: 0, selected: false, history: [] },
               { name: 'Água sem Gás',
-                total:  2.00, left:  2.00, paying: 0, selected: false },
+                total:  2.00, left:  2.00, paying: 0, selected: false, history: [] },
               { name: 'Salada com Pepino, Abóbora Seca, Rúcula e Alface',
-                total: 20.00, left: 20.00, paying: 0, selected: false },
+                total: 20.00, left: 20.00, paying: 0, selected: false, history: [] },
               { name: 'Suco de Laranja',
-                total:  5.50, left:  5.50, paying: 0, selected: false },
+                total:  5.50, left:  5.50, paying: 0, selected: false, history: [] },
               { name: 'Arroz Pilaf',
-                total: 18.95, left: 18.95, paying: 0, selected: false },
+                total: 18.95, left: 18.95, paying: 0, selected: false, history: [] },
               { name: 'Pudim de Chocolate com Coco',
-                total:  8.90, left:  8.90, paying: 0, selected: false }] },
+                total:  8.90, left:  8.90, paying: 0, selected: false, history: [] }] },
           { n: 2,
             dishes: [] },
           { n: 3,
@@ -40,27 +41,37 @@
           { n: 5,
             dishes: [
               { name: 'Farofa de Ovos',
-                total:  9.90, left:  9.90, paying: 0, selected: false },
+                total:  9.90, left:  9.90, paying: 0, selected: false, history: [] },
               { name: 'Feijão Tropeiro',
-                total: 19.65, left: 19.65, paying: 0, selected: false },
+                total: 19.65, left: 19.65, paying: 0, selected: false, history: [] },
               { name: 'Maionese de Batatas',
-                total:  9.90, left:  9.90, paying: 0, selected: false },
+                total:  9.90, left:  9.90, paying: 0, selected: false, history: [] },
               { name: 'Moqueca Cearense',
-                total: 31.00, left: 31.00, paying: 0, selected: false },
+                total: 31.00, left: 31.00, paying: 0, selected: false, history: [] },
               { name: 'Arroz Biro Biro',
-                total: 22.50, left: 22.50, paying: 0, selected: false },
+                total: 22.50, left: 22.50, paying: 0, selected: false, history: [] },
               { name: 'Carne de Sol',
-                total: 28.70, left: 28.70, paying: 0, selected: false },
+                total: 28.70, left: 28.70, paying: 0, selected: false, history: [] },
               { name: 'Suco de Melancia',
-                total:  5.50, left:  5.50, paying: 0, selected: false },
+                total:  5.50, left:  5.50, paying: 0, selected: false, history: [] },
               { name: 'Soda',
-                total:  4.00, left:  4.00, paying: 0, selected: false },
+                total:  4.00, left:  4.00, paying: 0, selected: false, history: [] },
               { name: 'Coca-Cola',
-                total:  4.00, left:  4.00, paying: 0, selected: false }] },
+                total:  4.00, left:  4.00, paying: 0, selected: false, history: [] }] },
           { n: 6,
             dishes: [] },
           { n: 7,
-            dishes: [] },
+            dishes: [
+              { name: 'Picadinho de Filé com Ovo, Arroz, Feijão, Banana da Terra  Batatas Chips',
+                total: 34.60, left: 34.60, paying: 0, selected: false, history: [] },
+              { name: 'Pastéis (8 unidades)',
+                total: 20.00, left: 20.00, paying: 0, selected: false, history: [] },
+              { name: 'Peixe do Dia Empanado',
+                total: 24.50, left: 24.50, paying: 0, selected: false, history: [] },
+              { name: 'Heineken',
+                total: 13.00, left: 13.00, paying: 0, selected: false, history: [] },
+              { name: 'Soda',
+                total:  4.00, left:  4.00, paying: 0, selected: false, history: [] }] },
           { n: 8,
             dishes: [] },
           { n: 9,
@@ -74,8 +85,6 @@
           { n: 13,
             dishes: [] },
           { n: 14,
-            dishes: [] },
-          { n: 15,
             dishes: [] }
         ]
       };
@@ -99,7 +108,7 @@ body {
   margin: 0;
   padding: 0;
 
-  //font-family: 'Open Sans', sans-serif;
+  font-family: Arial; 
   background-color: $app-back-color;
 }
 
