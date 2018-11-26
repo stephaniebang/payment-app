1129b9744ee00c2c04928cd3f649dca058e6269c
stephaniebang <ej.bang27@gmail.com>
2018-11-26 16:10:46 -0200

    Add modules and types

---
+++ src/store/types.js
@@ -0,0 +1,23 @@
+// Getters
+export const INDEX         = "index/INDEX";
+export const DISH          = "table/DISH";
+export const NUMBER_TABLES = "table/NUMBER_TABLES";
+export const NUMBER_DISHES = "table/NUMBER_DISHES";
+export const TABLE_N       = "table/TABLE_N";
+export const TABLE_PAYING  = "table/TABLE_PAYING";
+export const TABLE_STATUS  = "table/TABLE_STATUS";
+export const TABLE_TOTAL   = "table/TABLE_TOTAL";
+
+// Mutations
+export const MUTATE_UPDATE_INDEX          = "index/MUTATE_UPDATE_INDEX";
+export const MUTATE_PAY_LEFT              = "table/MUTATE_PAY_LEFT";
+export const MUTATE_PAY_SELECTED          = "table/MUTATE_PAY_SELECTED";
+export const MUTATE_UPDATE_ITEM_PAYMENT   = "table/MUTATE_UPDATE_ITEM_PAYMENT";
+export const MUTATE_UPDATE_ITEM_SELECTION = "table/MUTATE_UPDATE_ITEM_SELECTION";
+
+// Actions
+export const UPDATE_INDEX          = "index/UPDATE_INDEX";
+export const PAY_LEFT              = "table/PAY_LEFT";
+export const PAY_SELECTED          = "table/PAY_SELECTED";
+export const UPDATE_ITEM_PAYMENT   = "table/UPDATE_ITEM_PAYMENT";
+export const UPDATE_ITEM_SELECTION = "table/UPDATE_ITEM_SELECTION";
