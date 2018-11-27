14dfcacabf178d5ac462d355047489ef7f03deef
stephaniebang <ej.bang27@gmail.com>
2018-11-27 00:02:20 -0200

    Turn Header to a container composed by 2 components

---
+++ src/components/HeaderTitle.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="title">
+  <span v-if="ind < 0">MESAS</span>
+  <span v-else>MESA {{ ind+1 }}</span>
+</div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    // current table index
+    ind: {
+      type: Number
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "~styles/reference.scss";
+
+.title {
+  grid-area: title;
+
+  display: flex;
+  align-items: center;
+
+  color: $header-text-color;
+  font-size: 2.2em;
+}
+</style>
---
+++ src/containers/Header.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="header">
+  <header-title :ind="tableIndex"/>
+
+  <header-back v-if="tableIndex >= 0" class="back" @click.native="updateTableIndex(-1)"/>
+</div>
+</template>
+
+
+<script>
+import HeaderTitle from "../components/HeaderTitle.vue";
+import HeaderBack  from "../components/HeaderBack.vue";
+
+import { mapGetters, mapActions } from "vuex";
+import * as types from "../store/types"
+
+export default {
+  components: {
+    "header-back":  HeaderBack,
+    "header-title": HeaderTitle
+  },
+
+  computed: {
+    ...mapGetters({
+      tableIndex: types.INDEX
+    })
+  },
+
+  methods: {
+    ...mapActions({
+      updateTableIndex: types.UPDATE_INDEX
+    })
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import '~styles/reference.scss';
+
+.header {
+  grid-area: header;
+
+  display: grid;
+  grid-template-areas:
+    ". icon title";
+  grid-template-columns: 3% 40% 57%;
+
+  position: sticky;
+  top: 0;
+}
+</style>
